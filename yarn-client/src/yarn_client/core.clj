c7a14f1670953b280d4bef613dee67ad3d206d95
spiderdt <larluo@clojurians.org>
2016-11-01 00:20:13 +0800

    work backup for add container context

--- yarn-client/src/yarn_client/core.clj
+++ yarn-client/src/yarn_client/core.clj
@@ -3,9 +3,9 @@
             [clojure.tools.trace :refer [trace trace-forms dotrace]])
   (:import [org.apache.hadoop.yarn.client.api YarnClient]
            [org.apache.hadoop.conf Configuration]
-           [org.apache.hadoop.yarn.conf YarnConfiguration])  )
-
-
+           [org.apache.hadoop.yarn.conf YarnConfiguration]
+           [org.apache.hadoop.yarn.util Records]
+           [org.apache.hadoop.yarn.api.records ContainerLaunchContext])  )
 
 (set-level! :trace)
 (def conf (doto (new Configuration) (.set "yarn.resourcemanager.hostname" "192.168.1.3")) )
@@ -14,5 +14,16 @@
     (.init client (new YarnConfiguration conf)) 
     (.start client)
     (.getYarnClusterMetrics client)
-    (-> client .createApplication .getNewApplicationResponse )
+    (def app (-> client .createApplication))
+    (def app-id (-> app .getNewApplicationResponse .getApplicationId))
+
+    (def container-context (doto (Records/newRecord ContainerLaunchContext)
+                             (.setCommands ["hostname"])) )
+    (def app-context (.getApplicationSubmissionContext app))
+    
+    (doto app-context
+      (.setAMContainerSpec container-context)) 
+    
+        
+    (.stop client)
     )
