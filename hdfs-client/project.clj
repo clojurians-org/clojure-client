2b0a252ea3c5da3395c1ef2bec6651ab74552408
spiderdt <larluo@clojurians.org>
2016-11-15 01:21:22 +0800

    add protocol buffer

--- hdfs-client/project.clj
+++ hdfs-client/project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.hadoop/hadoop-common "2.7.3"]
-                 [org.apache.hadoop/hadoop-hdfs "2.7.3"]])
+                 [com.taoensso/timbre "4.7.4"]
+                 [com.fzakaria/slf4j-timbre "0.3.2"]
+                 [com.spiderdt.framework/spiderdt-client "0.1"]])
